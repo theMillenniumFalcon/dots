79e5cc8512b431857a4f1a56d14adf9840fefdde
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-11-18 19:49:55 +0530

    updated

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -2,6 +2,8 @@
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\nishank.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
 # Alias
 Set-Alias -Name vim -Value nvim
